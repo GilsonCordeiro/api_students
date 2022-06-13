da03c6bfcd2a7eaa40ed4757a98baf3ec53e4505
GilsonCordeiro <gil.silva.cord2021@gmail.com>
2022-06-13 18:43:02 -0300

    Criação do projeto com classe Student

---
+++ src/main/java/com/school/student/models/Student.java
@@ -0,0 +1,10 @@
+package com.school.student.models;
+
+public class Student {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String course;
+
+}

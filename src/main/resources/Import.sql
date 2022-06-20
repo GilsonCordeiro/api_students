94c05753b1dbec486144c63fbdebfbb4a3ed16eb
GilsonCordeiro <gil.silva.cord2021@gmail.com>
2022-06-20 13:51:47 -0300

    Adição de banco de dados

--- src/main/resources/Import.sql
+++ src/main/resources/Import.sql
@@ -1,3 +1,6 @@
-INSERT INTO tb_courses(course, monthly, institution) VALUES ('Sistema da Informação', 879.45, 'Anhanguera');
+INSERT INTO tb_courses(course, monthly, institution) VALUES ('Sistema da Informação', 879.45 , 'Católica');
+INSERT INTO tb_courses(course, monthly, institution) VALUES ('Engenheiro Civil', 1.345 , 'Santa Terezinha');
 
-INSERT INTO tb_students(course_id, name, email, password) VALUES (1, 'Catya Nadys', 'catya@yahoo.com', '1e3Ac32');
+INSERT INTO tb_students(course_id, name, email, password) VALUES (1, 'Catya Nadys', 'catyanadys@yahoo.com', '1e3Ac32');
+INSERT INTO tb_students(course_id, name, email, password) VALUES (2, 'Pietro Green', 'pietrogreen@yahoo.com', '4e3Ac54');
+INSERT INTO tb_students(course_id, name, email, password) VALUES (1, 'Nathália Pink', 'nathaliapink@yahoo.com', '2eGNc12');
